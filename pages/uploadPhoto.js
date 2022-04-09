7a222d90220089b01ab3e97df7f3986575fe6e1a
kent28808 <kktanoue@gmail.com>
2022-04-08 17:20:01 -0700

    Basic photo upload implemented

--- pages/uploadPhoto.js
+++ pages/uploadPhoto.js
@@ -1,18 +1,79 @@
 import Link from "next/link";
+import Head from 'next/head'
+import Uppy from '@uppy/core'
+import Tus from '@uppy/tus'
+import Statusbar from '@uppy/status-bar'
+import { Dashboard } from '@uppy/react'
+import '@uppy/core/dist/style.css'
+import '@uppy/dashboard/dist/style.css'
 
-const uploadPhotoScreen = () => {
+
+const uppy = new Uppy()
+	.use(Tus, {
+		endpoint: 'https://tusd.tusdemo.net/files/'
+	});
+
+uppy.on('complete', (result) => {
+	console.log('Upload complete!', result.successful)
+})
+
+
+export default function Home() {
   return (
-    <div>
-      <h1>Next, let's make sure your partner can see you.</h1>
-      <br></br>
-      <p>
-        Pick a moment from your photos to remind both of you the happy days.
-      </p>
+    <div className="container">
+			<Dashboard
+				uppy={uppy}
+			/>	
       <Link href='/previewPhoto' passHref>
         <button>Next</button>
       </Link>
-    </div>
-  );
-};
+  	</div>
+	)
+}
 
-export default uploadPhotoScreen;
+
+
+
+// Uppy Config
+// const uppy = new Uppy({
+//   id: 'uppy',
+//   autoProceed: false,
+//   allowMultipleUploadBatches: true,
+//   debug: false,
+//   restrictions: {
+//     maxFileSize: null,
+//     minFileSize: null,
+//     maxTotalFileSize: null,
+//     maxNumberOfFiles: null,
+//     minNumberOfFiles: null,
+//     allowedFileTypes: null,
+//     requiredMetaFields: [],
+//   },
+//   meta: {},
+//   onBeforeFileAdded: (currentFile, files) => currentFile,
+//   onBeforeUpload: (files) => {},
+//   locale: {},
+//   store: new DefaultStore(),
+//   logger: justErrorsLogger,
+//   infoTimeout: 5000,
+// })
+
+
+
+
+// const uploadPhotoScreen = () => {
+//   return (
+//     <div>
+//       <h1>Next, let's make sure your partner can see you.</h1>
+//       <br></br>
+//       <p>
+//         Pick a moment from your photos to remind both of you the happy days.
+//       </p>
+//       <Link href='/previewPhoto' passHref>
+//         <button>Next</button>
+//       </Link>
+//     </div>
+//   );
+// };
+
+// export default uploadPhotoScreen;
