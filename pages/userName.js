4e3fb30127b217ff493a9e59b7379e2e8f6f41f5
kent28808 <kktanoue@gmail.com>
2022-04-12 22:49:50 -0700

    added progress bar1 on userName

--- pages/userName.js
+++ pages/userName.js
@@ -1,13 +1,20 @@
+import Image from "next/image"
 import { useContext } from "react";
 import { MessageContext } from "../context/MessageContext"; // Wrap in {} b/c exported context w/o keyword default
 import GetFormData from "../components/Forms/GetFormData";
 import ButtonSymbol from "../components/ButtonSymbol";
+import progressBar1 from "../public/progress/progressbar_step1.png"
 
 export default function userNameScreen() {
   const { message } = useContext(MessageContext);
 
   return (
     <div className='container'>
+      <Image
+        src={progressBar1}
+        className='OB-image'
+        alt='progress bar1 logo'
+      />
       <h3>What's your name?</h3>
       <GetFormData dataType='text' msgKey='senderName' inputId='sender-name' />
       <p>{`${JSON.stringify(message)}`}</p>
