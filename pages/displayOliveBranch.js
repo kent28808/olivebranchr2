3defd4a07e824ac60010fdd54c41f28a31002fca
kent28808 <kktanoue@gmail.com>
2022-04-15 12:26:23 -0700

    line 10 commented out on displayOliveBranch.js

--- pages/displayOliveBranch.js
+++ pages/displayOliveBranch.js
@@ -7,15 +7,9 @@ export default function Displayolivebranch() {
 
   return (
     <div className='container'>
-<<<<<<< HEAD
-      <p>{`${JSON.stringify(ob)}`}</p>
-      <h1>{ob.senderName}</h1>
-      <Image src={ob.imgURL} className='preview-img' layout='fill'/>
-=======
       {/* <p className='body'>{`${JSON.stringify(ob)}`}</p> */}
       <h1 className='title'>{ob.senderName}</h1>
-      <Image src={ob.imgURL} className='preview-img' layout='fill' />
->>>>>>> f7dc97745700022d2b7e5acf63361437ad9b944b
+      <Image src={ob.imgURL} className='preview-img' alt="preview img" layout='fill' />
       <audio src={ob.blobURL} controls='controls' />
     </div>
   );
