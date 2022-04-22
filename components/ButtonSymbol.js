fe38692a5e67bafc9f28cc14f6599b30a4c7b50d
kent28808 <kktanoue@gmail.com>
2022-04-21 22:54:05 -0700

    Implemented consistent default cursors on all buttons

--- components/ButtonSymbol.js
+++ components/ButtonSymbol.js
@@ -7,12 +7,12 @@ export default function NextButton(props) {
   return (
     <button className='button-next' type={props.type}>
       {props.type === "submit" ? (
-        <Image src={arrow} alt='arrow icon' />
+        <Image src={arrow} alt='arrow icon'/>
       ) : (
         <Link href={props.innerRef} passHref>
-          <a>
-            <Image src={arrow} alt='arrow icon' />
-          </a>
+          <span>
+            <Image src={arrow} alt='arrow icon'/>
+          </span>
         </Link>
       )}
     </button>
