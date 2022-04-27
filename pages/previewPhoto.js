6c0b665794a7b220c2e69f04f4188b73f0f10aeb
kent28808 <kktanoue@gmail.com>
2022-04-27 12:31:41 -0700

    updated readme and added images to public

--- pages/previewPhoto.js
+++ pages/previewPhoto.js
@@ -21,7 +21,7 @@ export default function Previewphoto() {
       <Image
         src={message.imgURL}
         className='preview-img'
-        alt ="preview img"
+        alt ="preview"
         width='500px'
         height='500px'
       />
